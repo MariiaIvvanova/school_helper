6af0a698140b07f47cd59e7e349bdf82a6f0ef7a
MariiaIvvanova <ivanova_mariia36@mail.ru>
2025-03-04 20:44:12 +0300

    mvp mvp

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
-from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_gigachat.chat_models import GigaChat
+from src.llm.giga_chat import messages, HumanMessage, giga
 
-from src.config import Config
-
-config = Config()
-giga = GigaChat(
-    # Для авторизации запросов используйте ключ, полученный в проекте GigaChat API
-    credentials=config.GIGA_CHAT_AUTH_KEY,
-    verify_ssl_certs=False,
-)
-
-messages = [
-    SystemMessage(
-        content="""Ты — интеллектуальный литературный справочник. Пользователь вводит название книги, а ты предоставляешь информацию в следующем формате:
- 1. Название: [Оригинальное название + год публикации]
- 2. Автор: [ФИО автора и краткая биография]
- 3. Жанр: [Роман, философская литература, фантастика и т. д.]
- 4. Краткое описание: [2-3 предложения о сюжете]
- 5. Краткое содержание: [Основные события книги в сжатом виде 10 предложений]
- 6. Философские вопросы:
- • Какое главное послание этой книги?
- • Какие моральные или этические вопросы она поднимает?
- • Какую идею автор хотел донести до читателя?"""
-    )
-]
 
 while True:
     user_input = input("Пользователь: ")
--- tgbot_example.py
+++ tgbot_example.py
@@ -3,18 +3,27 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+from src.config import Config
+from src.llm.giga_chat import giga
+
+config = Config()
+TOKEN = config.TG_BOT_KEY
+
 # Включаем логгирование для удобства отладки
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-# Вставьте сюда ваш токен, полученный от BotFather
-TOKEN = "7605076216:AAEputBeLuKhx41s_b77dwcUu6K1rlygxEU"
 
 # Обработчик команды /start
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я бот. Чем могу помочь?")
+    # await update.message.reply_text("Привет! Я бот. Чем могу помочь?")
+    # print(update.message.text, update.message.from_user.username)
+    message = update.message.text.replace("/start ", "")
+    res = giga.invoke(message)
+    await update.message.reply_text(res.content)
+
 
 def main():
     # Создаём приложение (Application) с указанным токеном
